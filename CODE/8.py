bf70201d1dc3b43a685fa3c64c0db65fa43bdc13
IS1-4MJ <30614712+IS1-4MJ@users.noreply.github.com>
2022-06-04 07:46:54 -0500

    SEE LEFT: LEARN PYTHONISTA KEYBOARD MODULE
    PYTHONISTA FOR IOS...
    https://apps.apple.com/us/app/pythonista-3/id1085978097
    
    PYTHONISTA FOR ANDROID (NOT TESTED OR ENDORSED) https://baixarapk.gratis/en/app/1085978097/pythonista-3

---
+++ CODE/8.py
@@ -0,0 +1,29 @@
+import keyboard
+#print(keyboard.__dict__)
+#for k,v in keyboard.__dict__.items():
+#	print(k,v)
+
+#keyboard.backspace(10)
+#print(keyboard.get_appearance())
+#print(keyboard.get_document_id())
+#print(keyboard.get_input_context())
+#print(repr(keyboard.get_selected_text()))
+#keyboard.get_selected_text()
+#print(keyboard.get_text_replacements())
+#print(keyboard.has_full_access())
+#print(keyboard.has_text())# not work 1st time for pythonista tab
+#print(keyboard.insert_text())#print(keyboard.insert_text()) TypeError: Required argument 'text' (pos 1) not found
+#print(keyboard.insert_text("alpha"))
+#print(keyboard.is_keyboard())
+#keyboard.move_cursor(0)
+#keyboard.move_cursor(1)
+#keyboard.move_cursor(-1)
+#keyboard.play_input_click()
+#keyboard.set_view(mode="expanded")#NOTDO?
+
+#from time import sleep
+#J=2**5
+#K=1
+#[[keyboard.play_input_click(), sleep(.01/J/K)] for i in range(20*J)] #HOW FAST??? (CAN DO AT .01)!!! HOW MANY??? MANY! THE SAME PITCH UNTIL? 2**3 ish. K=10000 does not much
+
+#hruducndndjd
